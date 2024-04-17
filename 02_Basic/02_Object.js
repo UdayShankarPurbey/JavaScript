6b1d004f93fbca01e7293e385c68301ed97a6b05
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-17 13:03:46 +0530

    Objects and some of its Method

--- 02_Basic/02_Object.js
+++ 02_Basic/02_Object.js
@@ -35,3 +35,80 @@ user.greetingUser = function() {
 
 console.log(user.greeting());
 console.log(user.greetingUser());
+
+
+const regularUser = {
+   email : "admin@example.com",
+   fullName : {
+    userFullName : {
+        firstName : "John",
+        lastName : "Chaudhary",
+    }
+   }
+}
+
+console.log(regularUser.fullName?.userFullName.firstName);
+
+const obj1 = {
+    1 : "a",
+    2 : "b",
+}
+
+const obj2 = {
+    3 : "z",
+    4 : "y",
+}
+const obj3 = {
+    5 : "o",
+    6 : "n",
+}
+
+// const obj4 = Object.assign({}, obj1, obj2 , obj3)
+// console.log(obj4);
+
+const obj4 = {...obj1 , ...obj2 , ...obj3}
+console.log(obj4);
+
+const userData = [
+    {
+        name : "John",
+        age : 18,
+        location : "San Francisco",
+        email : "user@example.com",
+        isLoggedIn : false,
+    },
+    {
+        name : "Baibhavi",
+        age : 34,
+        location : "Tokoyo",
+        email : "baibhavi@comp.com",
+        isLoggedIn : true,
+    },
+    {
+        name : "raushan",
+        age : 27,
+        location : "Banaras",
+        email : "raushan@rdone.com",
+        isLoggedIn : false,
+    },
+    {
+        name : "Abhinav",
+        age : 16,
+        location : "London",
+        email : "abhi@mick.com",
+    }
+]
+
+const tinderUser = {}
+
+tinderUser.id = "fjkd232"
+tinderUser.name = "John"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("id"));
+
