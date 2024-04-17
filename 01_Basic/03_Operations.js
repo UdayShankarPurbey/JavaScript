62aed998aaab6f52019981ffa7c8dd358485822f
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-17 10:17:17 +0530

    Operators in Javascript

---
+++ 01_Basic/03_Operations.js
@@ -0,0 +1,24 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%3);
+
+
+let str1 = 'Uday'
+let str2 = 'Shankar'
+let str3 = str1 + ' ' + str2 + ' Purbey'
+console.log(str3);
+
+console.log("1"+2 + 1);
+console.log(2+ 2+"1");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
