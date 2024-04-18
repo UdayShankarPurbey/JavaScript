fbb67812a8f60c938fe0df70ab8f613b54335683
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-18 12:06:14 +0530

    while loop

---
+++ 03_iteration/02_whileLoop.js
@@ -0,0 +1,19 @@
+let index = 0;
+while (index <= 10) {
+    console.log(index);
+    index++;
+}
+
+let myArray = [ "flash" , "batman", "superman"]
+
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(myArray[arr]);
+    arr++;
+}
+
+score = 11;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
