52715b08a2a007ebf4b59b17acc8f099587a7e87
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-18 12:42:25 +0530

    Fro Each loop

---
+++ 03_iteration/05_ForEachLoop.js
@@ -0,0 +1,42 @@
+const coding = [ 'javascript', 'ruby', 'java', 'python', 'csharp' ];
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach((item) => {
+//     console.log(item);
+// })
+
+// function printData(data) {
+//     console.log(data);
+// }
+
+// coding.forEach(printData); //here just pass reference not exicute it.
+
+coding.forEach((item , index , data) => {
+    console.log(`${item} --- ${index} --- ${data}`);
+});
+
+const myCoding = [
+    {
+        languageName: 'javascript',
+        languageFileName: 'js'
+    },
+    {
+        languageName: 'java',
+        languageFileName: 'java'
+    },
+    {
+        languageName: 'python',
+        languageFileName: 'py'
+    },
+    {
+        languageName: 'c',
+        languageFileName: 'cpp'
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
